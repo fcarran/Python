e68c9c18e42b3fa69214888aa0df56c37b4af313
Fernando Carranza <fernandocarranza@Fernando-PersonalMacbook.local>
2018-06-13 22:40:31 -0700

    added description

--- diceSimulator.py
+++ diceSimulator.py
@@ -1,1 +1,7 @@
+# progam to simulate rolling a dice
+# Program will run, it randomly chooses a number between 1 and 6
+# program will print result
+# program will ask if you would like to run it again
+# HINT: set min and max number your dice can produce
+# HINT: want a function that randomly grabs a number from that range and prints it
 import random
